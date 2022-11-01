b44dc9ae37c990f655bab03455cf7fea858450cb
Jair <jairntonio21@hotmail.com>
2022-10-31 19:35:53 -0600

    qq

--- Actividad13iOSWeb/ViewController.swift
+++ Actividad13iOSWeb/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftView:UIView!
+    @IBOutlet weak var rigthView:UIView!
+    @IBOutlet weak var leadingConstraint:NSLayoutConstraint!
+    @IBOutlet weak var trailingConstraint:NSLayoutConstraint!
+
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func animacion( sener:UIButton){
+        
+    }
+    
+    @IBAction func buttonTapped(){
+        let vc = SFSafariViewController(url: URL(string: "https://www.rtve.es/playz/20210507/top-15-mejores-peliculas-terror-deberias-ver/2088886.shtml")!)
+        
+        present(vc,animated: true)
 
 }
-
+    
+}
