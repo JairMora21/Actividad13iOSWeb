fbfc1b139aabf15c433e3dd2104d641c1b5f3a3e
Jair <jairntonio21@hotmail.com>
2022-11-01 21:38:16 -0600

    final

--- Actividad13iOSWeb/ViewController.swift
+++ Actividad13iOSWeb/ViewController.swift
@@ -6,22 +6,22 @@
 //
 
 import UIKit
+//importamos para poder usar el buscador de safari
 import SafariServices
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var leftView:UIView!
-    @IBOutlet weak var rigthView:UIView!
+// variables para mover el constraint
     @IBOutlet weak var leadingConstraint:NSLayoutConstraint!
     @IBOutlet weak var trailingConstraint:NSLayoutConstraint!
 
 
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    
+    // esta funcion hace las animaciones de los UIView
     @IBAction func animacion( sener:UIButton){
         
         /*Portada
@@ -31,7 +31,7 @@ class ViewController: UIViewController {
         }
         */
         
-        
+        //anima el primer UIVew con una duracion de 4 segundos
         UIView.animate(withDuration: 4) {
             self.leadingConstraint.constant = 10
             self.view.layoutIfNeeded()
@@ -43,7 +43,7 @@ class ViewController: UIViewController {
                 }
         }
     }
-        
+        //anima el segundo UIVew con una duracion de 9 segundos para que primero pase UIView anterior y no se estorben
         /*introduccion
         UIView.animate(withDuration: 1) {
             self.trailingConstraint.constant = 10
